14c2c9fc0bdc2e98306dd01da03dc56e09f146da
maheye <u277721157@byui.edu>
2024-02-20 15:15:15 +0800

    updates

--- server.js
+++ server.js
@@ -11,42 +11,38 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app
-    .use(bodyParser.json())
-    .use(session({
-        secret: "secret",
-        resave: false,
-        saveUninitialized: true,
-    }))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
-        );
-        res.setHeader(
-            'Access-Control-Allow-Methods', 
-            'POST, GET, PUT, PATCH, OPTIONS, DELETE'
-        );
-        next();
-    })
-    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
-    .use(cors({ origin: '*'}))
-    .use("/", require("./routes/index.js"));
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
-    passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL
-    },
-    function(accessToken, refreshToken, profile, done) {
-        //User.findOrCreate({ githubId: profile.id }, function (err, user) {
-            return done(null, profile);
-        //})
-    }
-    ));
+app.use(cors({methods:['GET', 'POST', 'DELETE', 'UPDATE', 'PUT','PATCH']}))
+app.use(cors({origin: '*'}))
+app.use('/', require("./routes/index.js"));
+
+passport.use(new GitHubStrategy ({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done){
+    //User.findOrCreate({githubId: profile.id}, function(err,user){
+       return done(null,profile);
+    //})
+}));
 
 passport.serializeUser((user, done) => {
     done(null, user);
@@ -56,7 +52,6 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get('/', (req, res) => {
-
     if (req.session.user !== undefined) {
         const userid = req.session.user.id;
         const displayName = req.session.user.displayName;
@@ -79,7 +74,7 @@ app.get('/github/callback', passport.authenticate('github', {
     (req, res) => {
         req.session.user = req.user;
         res.redirect('/');
-    });
+});
 
 mongodb.initDb((err) => {
     if (err) {

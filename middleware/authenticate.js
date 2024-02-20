4e1afcd6b00f61f01a22d2cede20b0511cf78d41
maheye <u277721157@byui.edu>
2024-02-20 14:58:22 +0800

    updates

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,3 @@
-//...This is the middleware/authenticate.js file...
-
 const isAuthenticated = (req, res, next) => {
     if (req.session.user === undefined){
         return res.status(401).json("You do not have access.");
